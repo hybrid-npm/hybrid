89ea8681bbf7851054f3726791e54c9a2c35480b
Ian Hunter <ian@ianh.xyz>
2026-04-28 08:11:46 -0500

    fix: simplify flyio Dockerfile and scheduler build for CI

--- packages/scheduler/tsup.config.ts
+++ packages/scheduler/tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsup"
 export default defineConfig({
 	entry: ["src/index.ts"],
 	format: ["cjs", "esm"],
-	dts: true,
+	dts: process.env.CI ? false : true,
 	splitting: false,
 	sourcemap: true,
 	clean: true,
